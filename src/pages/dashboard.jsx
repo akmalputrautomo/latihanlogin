12d3ffe052cc4cb88d6d1664e4f51d5067d7f091
akmalputrautomo <apu.utomo@gmail.com>
2023-10-16 18:21:38 +0700

    revisi

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,10 +3,10 @@ import { CookieKeys, CookieStorage } from "../utils/cookies";
 import { useGetDataUser } from "../services/get-user";
 
 export const Dashboard = () => {
-  const { data: Paijosalto, error, isError, status } = useGetDataUser({});
+  const { data: Paijosalto } = useGetDataUser({});
 
   return (
-    <div className="flex items-center justify-center flex-col ">
+    <div className="flex items-center justify-center flex-col h-screen ">
       <h1 className="text-2xl font-bold mb-4">DASHBOARD DERR</h1>
       <button
         className="py-2 px-4 bg-red-500 text-white rounded-md"
