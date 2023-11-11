8793351a10cf8ae0f952303d6bca18808fac2095
akmalputrautomo <apu.utomo@gmail.com>
2023-11-11 19:37:57 +0700

    revisi hapus code yang ga kepake

--- src/pages/latihanresponsive.jsx
+++ src/pages/latihanresponsive.jsx
@@ -2,20 +2,6 @@ import React from "react";
 
 export const Latihanresponsive = () => {
   return (
-    // <div className="w-full h-full text-center text-white p-4">
-    //   <h1 className="bg-red-500 w-full h-32 flex justify-center items-center">MERAH</h1>
-    //   <div className="flex tablet:flex-col-reverse desktop:flex">
-    //     <h1 className="bg-purple-500 w-1/2 h-[30rem] flex items-center justify-center m-2">UNGU</h1>
-    //     <div className="flex-col w-full">
-    //       <h1 className="bg-green-500 w-4/4 h-2/4 flex items-center justify-center m-2">HIJAU</h1>
-    //       <div className="flex h-full">
-    //         <h1 className="bg-blue-500 w-1/2 h-[13.5rem] flex items-center justify-center m-2">BIRU</h1>
-    //         <h1 className="bg-pink-500 w-1/2 h-[13.5rem] flex items-center justify-center m-2">PINK</h1>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="w-full h-full text-center text-white p-4">
       <div className="mobile:justify-end mobile:flex">
         <h1 className="bg-red-500 w-full h-32 flex justify-center items-center tablet:h-[5rem] tablet:w-full mobile:w-1/4">MERAH</h1>
